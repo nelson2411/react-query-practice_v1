fc7de0dd300fa0f8f688c4adf6d528a5c0b7c966
Nelson Rosales <nelson.rosales83@gmail.com>
2023-08-10 09:30:33 +0200

    feat: Axios API

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
+import { gitHubApi } from "../../api/gitHubApi"
+import { Label } from "../interfaces/label"
+
+const getLabels = async (): Promise<Label[]> => {
+  const { data } = await gitHubApi.get<Label[]>("/labels")
+  console.log("data", data)
+  return data
+}
 
 export const LabelPicker = () => {
+  const labelsQuery = useQuery(["labels"], getLabels)
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
+      >
+        Primary
+      </span>
     </div>
   )
 }
