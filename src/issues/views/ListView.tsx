06c1c68bc365d0caf53d0642cc9e96028be2f08e
Nelson Rosales <nelson.rosales83@gmail.com>
2023-08-12 12:08:04 +0200

    feat: labelPicker and css

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,17 +1,30 @@
-import { IssueList } from '../components/IssueList';
-import { LabelPicker } from '../components/LabelPicker';
-
+import * as React from "react"
+import { IssueList } from "../components/IssueList"
+import { LabelPicker } from "../components/LabelPicker"
 
 export const ListView = () => {
+  const [selectedLabels, setSelectedLabels] = React.useState<string[]>([])
+
+  const onLabelChanged = (labelName: string) => {
+    /*
+    If the label is already selected, remove it from the array of selected labels.
+    */
+    selectedLabels.includes(labelName)
+      ? setSelectedLabels(selectedLabels.filter((label) => label !== labelName))
+      : setSelectedLabels([...selectedLabels, labelName])
+  }
+
   return (
     <div className="row mt-5">
-      
       <div className="col-8">
         <IssueList />
       </div>
-      
+
       <div className="col-4">
-        <LabelPicker />
+        <LabelPicker
+          selectedLabels={selectedLabels}
+          onChange={(labelName) => onLabelChanged(labelName)}
+        />
       </div>
     </div>
   )
