c11cb2d51ad18b33d841e8e356a20d04ba77153b
Nelson Rosales <nelson.rosales83@gmail.com>
2023-08-18 18:24:54 +0200

    feat: fetch comments

--- src/issues/hooks/useIssue.ts
+++ src/issues/hooks/useIssue.ts
@@ -8,8 +8,23 @@ const getIssueInfo = async (issueNumber: number): Promise<Issue> => {
   return data
 }
 
+const getIssueComments = async (issueNumber: number) => {
+  const { data } = await gitHubApi.get(`/issues/${issueNumber}/comments`)
+  return data
+}
+
 export const useIssue = (issueNumber: number) => {
-  const Issueuery = useQuery(["issue", issueNumber], () =>
+  const issueQuery = useQuery(["issue", issueNumber], () =>
     getIssueInfo(issueNumber)
   )
+
+  const commentsQuery = useQuery(
+    ["issue", issueNumber, "comments"],
+    () => getIssueComments(issueQuery.data!.number),
+    {
+      enabled: issueQuery.data !== undefined,
+    }
+  )
+
+  return { issueQuery, commentsQuery }
 }
